8f3955cd77b048c66289d8d965fa6d0d7efc308f
Konrad Rotkiewicz <konrad.rotkiewicz@stxnext.pl>
2012-11-21 19:37:18 +0100

    init

---
+++ pyramid_autoroute/__init__.py
@@ -0,0 +1,82 @@
+import inspect
+import re
+import sys
+from random import choice
+from string import letters
+from pyramid.path import caller_package
+
+from pyramid.exceptions import ConfigurationError
+
+class FakeConfig(object):
+    def __init__(self, application_package):
+        self.application_package = application_package
+        self.views = []
+
+    def with_package(self, *arg, **kwargs):
+        return self
+
+    def add_view(self, *args, **kwargs):
+        route_name = kwargs.get('route_name')
+        view = kwargs['view']
+        self.views.append((route_name, view))
+
+
+class RouteResolver(object):
+
+    def __init__(self, config, root_module, views):
+        self.root_module = root_module
+        self.config = config
+        self.views = views
+
+    def resolveAll(self):
+        resolved = []
+        longest_len = 0
+        for view_name, view in self.views:
+            result = self.resolve(view_name, view)
+            if result:
+                maybe_longest_len = len(result[0])
+                if maybe_longest_len > longest_len:
+                    longest_len = maybe_longest_len
+                resolved.append(result)
+
+
+        print '\nAuto generated routes:\n'
+        print 'Name'.ljust(longest_len+6, ' '), 'Path'
+        print '----------------------------------------------------------------'
+        for view_name, path in resolved:
+            print view_name.ljust(longest_len+6, ' '), path
+
+        print '\n'
+
+
+    def get_callable_name(self, callable):
+        if inspect.isfunction(callable):
+            return callable.__module__, callable.func_name.lower()
+        elif inspect.isclass(callable):
+            return callable.__module__, callable.__name__.lower()
+        else:
+            raise NotImplementedError('Not implemented')
+
+    def resolve(self, name, callable):
+        callable_module, callable_name = self.get_callable_name(callable)
+        if not self.root_module in callable_module:
+            return
+
+        pos = callable_module.find(self.root_module)
+        rest = callable_module[pos:].lstrip(self.root_module).replace('.', '/')
+
+        path = '%s/%s' % (rest, callable_name)
+        if name:
+            self.config.add_route(name, path)
+            return name, path
+
+
+
+def includeme(config):
+    root_module = config.registry.settings['pyramid.autoroute.root_module']
+    caller = caller_package(level=3)
+    fake_config = FakeConfig(caller)
+    scanner = config.venusian.Scanner(config=fake_config)
+    scanner.scan(caller)
+
+    RouteResolver(config, root_module, fake_config.views).resolveAll()
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+install_requires=[
+    'pyramid>=1.0a10',
+    ]
+
+setup(name='pyramid_autoroute',
+      version='0.1',
+      description='Pyramid addon to auto detects views',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Framework :: Pyramid",
+        ],
+      keywords='web wsgi pylons pyramid',
+      author="",
+      author_email="",
+      url="http://docs.pylonsproject.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = install_requires,
+      entry_points = """
+      """
+      )
+
